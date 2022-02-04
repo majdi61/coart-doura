37da691cf237475ee8ec7061225f55cee6bc7784
Maazoun majdi <maazoun.majdiii@gmail.com>
2022-02-04 22:02:29 +0100

    Mark.

---
+++ src/main/java/com/coart/doura/domain/Mark.java
@@ -0,0 +1,28 @@
+package com.coart.doura.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "mark")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Mark {
+    @Transient
+    public static final String SEQUENCE_NAME_UIID = "mark_sequence_uiid";
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private long uiid;
+
+
+}
